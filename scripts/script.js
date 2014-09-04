c55435ee54b653ab99a18c88463f0df4b029bfc0
Sashok121 <kindras1@rambler.ru>
2014-09-04 11:12:28 +0300

    изменения

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,63 @@
 (function(){
 	// Создание обьекта Монстры
-	var Monstrs = {
-        name: "",
-        head: "",
-        uron:"",
-		sayName : function(){
-			console.log("My name is ", this.name);
-		},
-		shut : function(nameMonstr, shutName){
-			console.log("------------------------------------------------------------------");
-			console.log(this.name, " ", shutName, " ", nameMonstr.name);
-			if((nameMonstr.head) > 0){
-				nameMonstr.head -= this.uron;
-				if(nameMonstr.head > 0)
-					console.log(nameMonstr.name, " head is ", nameMonstr.head);
-				else
-					console.log("You killed ", nameMonstr.name);
-			}
+	function Monstrs() {
+        this.name = "";       
+    }
+    Monstrs.prototype.head = "";
+    Monstrs.prototype.uron = "";
+    Monstrs.prototype.sayName = function(){
+		console.log("My name is ", this.name);
+	};
+	Monstrs.prototype.shut = function(nameMonstr, shutName){
+		console.log("------------------------------------------------------------------");
+		console.log(this.name,  " ", shutName, " ", nameMonstr.name);
+		if((nameMonstr.head) > 0){
+			nameMonstr.heads(this.uron);
+			if(nameMonstr.head > 0)
+				console.log(nameMonstr.name, " head is ", nameMonstr.head);
 			else
-				console.log(nameMonstr.name, " is dead");
+				console.log("You killed ", nameMonstr.name);
 		}
-    };
+		else
+			console.log(nameMonstr.name, " is dead");
+	};
+	Monstrs.prototype.heads = function(uron){
+		this.head -= uron;
+	};
+    
     
   
-	var catMonstrs = function(obj){
-		this.name = obj.name || "";
-		this.level = 50;
-		this.head = 50;
-		this.uron = 5;
-		this.scratch = function(nameMonstr){
+	function catMonstrs(obj){
+		Monstrs.call(this);
+		this.name = obj.name || "";		
+	};	
+	catMonstrs.prototype = Object.create(Monstrs.prototype);	
+	catMonstrs.prototype.head = 50;
+	catMonstrs.prototype.uron = 5;
+	catMonstrs.prototype.scratch = function(nameMonstr){
 			this.shut(nameMonstr, "scratch");
 			};
-		};	
-	catMonstrs.prototype = Monstrs;	
-
+	catMonstrs.prototype.constructor = catMonstrs;	
+	
 	//Создание обьекта Птицемонстров
-	var birdMonstrs = function(obj){
-		this.name = obj.name || "";
-		this.level =60;
-		this.head = 60;
-		this.uron = 3;
-		this.bite = function(nameMonstr){
+	function birdMonstrs(obj){
+		Monstrs.call(this);
+		this.name = obj.name || "";		
+	};	
+	birdMonstrs.prototype = Object.create(Monstrs.prototype);	
+	birdMonstrs.prototype.head = 60;
+	birdMonstrs.prototype.uron = 3;
+	birdMonstrs.prototype.bite = function(nameMonstr){
 			this.shut(nameMonstr, "bite");
-			}
-		};	
-	birdMonstrs.prototype = Monstrs;
+			};
+	birdMonstrs.prototype.constructor = catMonstrs;
 	//-----------------------------------
 	var catMonstrs1 = new catMonstrs({name:"catMonstrs1"});
 	
 	//-----------------------------------
 	var catMonstrs2 = new catMonstrs({name:"catMonstrs2"});
+	
+	
 	
 	//-----------------------------------
 	var catMonstrs3 = new catMonstrs({name:"catMonstrs3"});
@@ -79,6 +87,6 @@
 	catMonstrs1.scratch(catMonstrs4);
 	catMonstrs2.scratch(catMonstrs4);
 	
-	
+	console.log(catMonstrs2);
 	birdMonstrs3.sayName();
 }());
